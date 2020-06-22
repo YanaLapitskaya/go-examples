30fe54dd6fcb7ed17dc29c6f49b86387f957252f
Yana Lapitskaya <yanalapitskaya@Yanas-MacBook-Pro.local>
2020-06-22 23:36:21 +0300

    added base rest api for tasks

---
+++ final_project/configs/db.configs.go
@@ -0,0 +1,34 @@
+package configs
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+var Db *sql.DB
+
+const (
+	host     = "final_project_db_1"
+	port     = 5432
+	user     = "postgres-dev"
+	password = "postres-dev-pass"
+	dbname   = "dev"
+)
+
+func InitDB() {
+	var err error
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	Db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	err = Db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+}
